83343ee481dae7affcfb199eef3b205bcab3b1d2
rebelice <yangrebelice@gmail.com>
2019-05-29 13:35:38 +0800

    flask & couchDB can init

--- src/server/routes.py
+++ src/server/routes.py
@@ -1,7 +1,25 @@
 from server import server
+from cloudant.client import CouchDB
 
 
 @server.route('/')
 @server.route('/index')
 def index():
     return 'Hello World!'
+
+
+@server.route('/init')
+def init():
+    client = CouchDB('admin', '123456', url='http://127.0.0.1:5984',
+                     connect=True,
+                     auto_renew=True)
+
+    db = client['agileffy_users']
+
+    if not db.exists():
+        db = client.create_database('agileffy_users')
+
+    if db.exists():
+        return 'SUCCESS!!'
+    else:
+        return 'ERROR:Can not create database!'
