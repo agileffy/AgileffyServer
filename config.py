86546e45f5d2cdcb52443b40d526141deb76bb16
rebelice <yangrebelice@gmail.com>
2019-06-09 17:46:03 +0800

    https test v1.1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 username = 'admin'
 password = '123456'
 database = 'agileffy_users'
-url = 'http://127.0.0.1:5984'
+url = 'https://agileffy.info:5984'
 TIMEOUT = 10
--- test/testlogin.py
+++ test/testlogin.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/api/login'
+url = 'https://agileffy.info/api/login'
 d = '{"username":"rebel1", "password":"123456"}'.encode()
 
 r = requests.post(url, data=d)
