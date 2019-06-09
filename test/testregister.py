f485cb9bc2cf7abdb390c9eca1a44df9d1c719a1
rebelice <yangrebelice@gmail.com>
2019-06-09 16:54:24 +0800

    https!

--- test/testregister.py
+++ test/testregister.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://127.0.0.1:5000/api/register'
+url = 'http://108.61.183.99/api/register'
 d = '{"username":"rebel","password":"123456","email" : "cylnb@cylnb.com"}'.encode()
 
 print(d)
